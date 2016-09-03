b7678f86e83afc7aa5829822e028e6cff90cdf0c
谢爽 <394229785@qq.com>
2016-09-03 22:38:39 +0800

    reconfig dir

---
+++ mycode/002 Add Two Numbers/app.js
@@ -0,0 +1,22 @@
+
+// Definition for singly-linked list.
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+    var left = 0,
+        result = {
+            val: 0,
+            next: null
+        };
+    while (l1.next != null) {
+
+    }
+};
